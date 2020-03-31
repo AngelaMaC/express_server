28eca4dd3cf2e5c14df587691e6db5c6df241a2d
Angela Canning <amc@angelacanning.com>
2020-03-31 14:03:44 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '618088598787686',
+        clientSecret: 'a821162ed22559833453d32ca8fdb5f7'
+    }
 }
